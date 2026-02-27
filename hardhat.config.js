170cce521280d48770efd0817be95dd4e056dd7c
Afoxcute <jwavolabilove16@gmail.com>
2026-02-27 19:38:07 +0100

    change network to FlowEVM

---
+++ hardhat.config.js
@@ -0,0 +1,40 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+require("@nomicfoundation/hardhat-toolbox-viem");
+require("@nomicfoundation/hardhat-verify");
+const config_1 = require("hardhat/config");
+if (!config_1.vars.has("DEPLOYER_PRIVATE_KEY")) {
+    console.error("Missing env var DEPLOYER_PRIVATE_KEY");
+}
+const deployerPrivateKey = config_1.vars.get("DEPLOYER_PRIVATE_KEY");
+const config = {
+    solidity: "0.8.24",
+    networks: {
+        flowTestnet: {
+            url: "https://testnet.evm.nodes.onflow.org",
+            accounts: [deployerPrivateKey],
+            timeout: 120000, // 120 seconds
+        },
+    },
+    etherscan: {
+        apiKey: {
+            flowTestnet: "YOU_CAN_COPY_ME",
+        },
+        customChains: [
+            {
+                network: "flowTestnet",
+                chainId: 545,
+                urls: {
+                    apiURL: "https://evm-testnet.flowscan.io/api",
+                    browserURL: "https://evm-testnet.flowscan.io",
+                },
+            },
+        ],
+    },
+    sourcify: {
+        // Disabled by default
+        // Doesn't need an API key
+        enabled: false,
+    },
+};
+exports.default = config;
---
+++ hardhat.config.ts
@@ -0,0 +1,44 @@
+import "@nomicfoundation/hardhat-toolbox-viem";
+import "@nomicfoundation/hardhat-verify";
+import type { HardhatUserConfig } from "hardhat/config";
+import { vars } from "hardhat/config";
+
+if (!vars.has("DEPLOYER_PRIVATE_KEY")) {
+  console.error("Missing env var DEPLOYER_PRIVATE_KEY");
+}
+
+const deployerPrivateKey = vars.get("DEPLOYER_PRIVATE_KEY");
+
+const config: HardhatUserConfig = {
+  solidity: "0.8.24",
+
+  networks: {
+    flowTestnet: {
+      url: "https://testnet.evm.nodes.onflow.org",
+      accounts: [deployerPrivateKey],
+      timeout: 120000, // 120 seconds
+    },
+  },
+  etherscan: {
+    apiKey: {
+      flowTestnet: "YOU_CAN_COPY_ME",
+    },
+    customChains: [
+      {
+        network: "flowTestnet",
+        chainId: 545,
+        urls: {
+          apiURL: "https://evm-testnet.flowscan.io/api",
+          browserURL: "https://evm-testnet.flowscan.io",
+        },
+      },
+    ],
+  },
+  sourcify: {
+    // Disabled by default
+    // Doesn't need an API key
+    enabled: false,
+  },
+};
+
+export default config;
